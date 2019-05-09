65114dedb4d0312f9f9aa75f241d6e998b2721e8
bp2008 <bp2008@users.noreply.github.com>
2019-05-08 22:23:35 -0600

    Misc

--- ImgurWin/ImgurWin/SetTimeout.cs
+++ ImgurWin/ImgurWin/SetTimeout.cs
@@ -12,8 +12,8 @@ namespace ImgurWin
 		/// <summary>
 		/// Invokes on the GUI thread the specified action after the specified timeout.
 		/// </summary>
-		/// <param name="TheAction"></param>
-		/// <param name="Timeout"></param>
+		/// <param name="TheAction">The action to run.</param>
+		/// <param name="Timeout">Values less than 0 are treated as 0. If the Timeout value is 0 for an OnGui operation, the invoke process will start before this method returns, making it impossible to cancel the operation.</param>
 		/// <returns></returns>
 		public static TimeoutHandle OnGui(Action TheAction, int Timeout, Form formForInvoking)
 		{
@@ -22,18 +22,18 @@ namespace ImgurWin
 		/// <summary>
 		/// Invokes on a background thread the specified action after the specified timeout.
 		/// </summary>
-		/// <param name="TheAction"></param>
-		/// <param name="Timeout"></param>
+		/// <param name="TheAction">The action to run.</param>
+		/// <param name="Timeout">Values less than 0 are treated as 0.</param>
 		/// <returns></returns>
 		public static TimeoutHandle OnBackground(Action TheAction, int Timeout)
 		{
 			return _internal_setTimeout(TheAction, Timeout, false, null);
 		}
 		/// <summary>
-		/// Invokes a call to setTimeout_Background on the Gui Thread, so that other UI events have a chance to finish first. This timeout will not be cancelable.
+		/// Invokes a call to SetTimeout.OnBackground on the Gui Thread, so that other UI events have a chance to finish first. This timeout will not be cancelable.
 		/// </summary>
-		/// <param name="TheAction"></param>
-		/// <param name="Timeout"></param>
+		/// <param name="TheAction">The action to run.</param>
+		/// <param name="Timeout">Values less than 0 are treated as 0.</param>
 		/// <returns></returns>
 		public static void AfterGuiResumesThenOnBackground(Action TheAction, int Timeout, Form formForInvoking)
 		{
@@ -44,32 +44,42 @@ namespace ImgurWin
 		}
 		private static TimeoutHandle _internal_setTimeout(Action TheAction, int Timeout, bool invokeOnGuiThread, Form formForInvoking)
 		{
+			if (Timeout < 0)
+				Timeout = 0;
 			TimeoutHandle cancelHandle = new TimeoutHandle();
-			Thread t = new Thread(
-				() =>
-				{
-					try
+			if (invokeOnGuiThread && Timeout == 0)
+				formForInvoking.Invoke(TheAction);
+			else
+			{
+				Thread t = new Thread(
+					() =>
 					{
-						if (cancelHandle.Wait(Timeout))
-							return;
-						if (invokeOnGuiThread)
-							formForInvoking.Invoke(TheAction);
-						else
-							TheAction();
+						try
+						{
+							if (cancelHandle.Wait(Timeout))
+								return;
+							if (invokeOnGuiThread)
+								formForInvoking.Invoke(TheAction);
+							else
+								TheAction();
+						}
+						catch (ThreadAbortException) { throw; }
+						catch (Exception ex)
+						{
+							Logger.Debug(ex);
+							Main.MessageBoxShow(ex.ToString());
+						}
 					}
-					catch (ThreadAbortException) { }
-					catch (Exception ex)
-					{
-						Logger.Debug(ex);
-						Main.MessageBoxShow(ex.ToString());
-					}
-				}
-			);
-			t.Name = "Timeout";
-			t.IsBackground = true;
-			t.Start();
+				);
+				t.Name = "Timeout";
+				t.IsBackground = true;
+				t.Start();
+			}
 			return cancelHandle;
 		}
+		/// <summary>
+		/// Allows cancellation of a scheduled operation up until the point that it begins. There is no confirmation if the cancelation was successful.
+		/// </summary>
 		public class TimeoutHandle
 		{
 			private EventWaitHandle ewh = new EventWaitHandle(false, EventResetMode.ManualReset);
@@ -83,7 +93,7 @@ namespace ImgurWin
 			/// <summary>
 			/// Waits up to the specified number of milliseconds and returns early with a value of true if the Cancel method was called during this time. Returns false at the end of the waiting period if not canceled.
 			/// </summary>
-			/// <param name="ms">The number of milliseconds to wait.</param>
+			/// <param name="ms">The number of milliseconds to wait, or -1 to wait indefinitely.</param>
 			/// <returns></returns>
 			internal bool Wait(int ms)
 			{
